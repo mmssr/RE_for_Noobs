6ce06231ae60ce9081d58f0209d1cfb98aa33b9d
0x6D6172636F <marcamesser@gmail.com>
2022-06-08 16:20:53 -0400

    Update RE Beginners 1.cpp

--- RE Beginners 1.cpp
+++ RE Beginners 1.cpp
@@ -3,16 +3,23 @@
 
 int main()
 {
+    /* declare 10 character variable to store
+    our string */
     char cdkey[10];
-    printf("****************************\n");
+    
+    /* yay banners */
+    printf("****************************\n"); 
     printf("*****Anti-Piracy Kit v1*****\n");
     printf("****************************\n");
     printf("Welcome to CTF Exercise 1!\n");
     printf("What is the cd-key?\n");
-
+    
+    /* read for user input, repeat to user */
     scanf("%s", cdkey);
     printf("You entered %s.\n", cdkey);
 
+    /* does this string match the key? if yes,
+    thanks for buying. if no, reprompt */
     int result = strcmp(cdkey, "f1rst_pwn");
     if (result == 0) {
         printf("Thanks for buying!\nClose this window when ready.\n");
@@ -23,5 +30,5 @@ int main()
         }
     }
     printf("Incorrect, try again!\n\n");
-    main();
+    main(); // restart since incorrect
 }
