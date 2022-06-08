139c5a1877f4079fd76c28b7709b2aa787bf32ff
0x6D6172636F <marcamesser@gmail.com>
2022-06-08 16:21:57 -0400

    Update RE Beginners2.cpp

--- RE Beginners2.cpp
+++ RE Beginners2.cpp
@@ -3,16 +3,23 @@
 
 int main()
 {
+    /* declare 11 character variable to store
+    our string */
     char cdkey[11];
+    
+    /* yay banners */
     printf("****************************\n");
     printf("*****Anti-Piracy Kit v2*****\n");
     printf("****************************\n");
     printf("Welcome to CTF Exercise 2!\n");
     printf("What is the cd-key?\n");
 
+    /* read for user input, repeat to user */
     scanf("%s", cdkey);
     printf("You entered %s.\n", cdkey);
 
+    /* does this reversed array match the key? if yes,
+    thanks for buying. if no, reprompt */
     char correctkey[11] = { '3','2','1','d','r','o','w','s','s','a','p'};
     int result = strcmp(cdkey, _strrev(correctkey));
     if (result == 0) {
@@ -25,5 +32,5 @@ int main()
     }
 
     printf("Incorrect, try again!\n\n");
-    main();
+    main(); // restart since incorrect
 }
